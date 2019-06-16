57f9b5155deaa217200ddd60a55e0995c692c78e
AlanMcClenaghan <alandmcclenaghan@icloud.com>
2019-06-16 21:51:01 +0100

    Data Validation and Sanitization.

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
   useNewUrlParser: true,
@@ -7,25 +8,58 @@ mongoose.connect("mongodb://127.0.0.1:27017/task-manager-api", {
 
 const User = mongoose.model("User", {
   name: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error("Email is invalid");
+      }
+    }
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {
+      if (value.toLowerCase().includes("password")) {
+        throw new Error('Password cannot include "password".');
+      }
+    }
   },
   age: {
-    type: Number
+    type: Number,
+    default: 0,
+    validate(value) {
+      if (value < 0) {
+        throw new Error("Age must be a postive number.");
+      }
+    }
   }
 });
 
 const Task = mongoose.model("Task", {
   description: {
-    type: String
+    type: String,
+    required: true,
+    trim: true
   },
   completed: {
-    type: Boolean
+    type: Boolean,
+    required: false,
+    default: false
   }
 });
 
 const task = new Task({
-  description: "Walk the dog",
-  completed: false
+  description: "   Feed the cat   "
 });
 
 task
@@ -38,8 +72,9 @@ task
   });
 
 // const me = new User({
-//   name: "Alan",
-//   age: 47
+//   name: "  Alan   ",
+//   password: "Password123",
+//   email: " alanDmcclenaghan@icloud.COM  "
 // });
 
 // me.save()
